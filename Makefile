94e470796f5583c973463b343814d0563823050b
Anderson <alebronjordan06@gmail.com>
2024-10-27 18:32:53 -0400

    se creo el codigo para manejar los textos del programa.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ ASM = nasm
 LINKER = gcc
 ASM_FLAGS = -f elf64
 
-OBJS = build/main.o build/math.o build/io.o build/error_handler.o build/messages.o
+OBJS = build/main.o build/math.o build/io.o build/error_handler.o build/textoarchivo.o
 
 all: calculadora
 
@@ -21,8 +21,8 @@ build/io.o: requerimientos/io.asm
 build/error_handler.o: requerimientos/error_handler.asm
 	$(ASM) $(ASM_FLAGS) -o build/error_handler.o requerimientos/error_handler.asm
 
-build/messages.o: include/messages.asm
-	$(ASM) $(ASM_FLAGS) -o build/messages.o include/messages.asm
+build/textoarchivo.o: requerimientos/textoarchivo.asm
+	$(ASM) $(ASM_FLAGS) -o build/textoarchivo.o requerimientos/textoarchivo.asm
 
 clean:
 	rm -f build/*.o calculadora
---
+++ requerimientos/textoarchivo.asm
@@ -0,0 +1,43 @@
+section .data
+global mensaje_num    
+mensaje_num db "Ingrese un numero: ", 0
+
+global mensaje_op     
+mensaje_op db "", 10, "Ingrese una de estas operaciones: (+, -, *, /, %)", 10, "o escriba 'q' para salir: ", 0
+
+global mensaje_inicio_ciclo       
+mensaje_inicio_ciclo db "", 10, "calculadora", 10, 10, "hecho por mi abuela", 10, "2024 todos los derechos reservados.", 10, 10, 0
+
+global mensaje_ciclo       
+mensaje_ciclo db "", 10, 10, "... Continuando. ", 10, 10, 10, 0
+
+global error_msg       
+error_msg db "",10 ,"[!] Ha ocurrido un error.", 10, 0
+
+global nueva_linea
+nueva_linea db " ", 0
+
+global msg_error_div
+msg_error_div db "",10 , "[!] No se puede dividir por cero.", 10, 0
+
+; Mensaje para math.asm
+global mensaje_suma
+mensaje_suma db "El resultado de la suma es: %lld", 10, 0
+
+global mensaje_resta
+mensaje_resta db "El resultado de la resta es: %lld", 10, 0
+
+global mensaje_multiplicacion
+mensaje_multiplicacion db "El resultado de la multiplicación es: %lld", 10, 0
+
+global mensaje_division
+mensaje_division db "El resultado de la división es: %lld", 10, 0
+
+global mensaje_modulo
+mensaje_modulo db "El resultado del módulo es: %lld", 10, 0
+
+global mensaje_operacion_invalida
+mensaje_operacion_invalida db "", 10, "[!] No se ha seleccionado ninguna operación válida.", 10, 10, 10, 0  ; Agregando salto de línea
+
+global mensaje_error_num
+mensaje_error_num db "",10, "[!] No se ingreso un numero. ", 10, 10, 0
